a7cf4291cf251adc38cad9310b1e727372b162d4
paeanoo <2488504316@qq.com>
2025-10-24 08:44:10 +0800

    部署

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  base: './', // 使用相对路径，解决部署路径问题
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
